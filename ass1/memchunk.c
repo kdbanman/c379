141ef4c472adf1e7ccca1c983cc5ec7aedcbeaf4
kdbanman <kirby.banman@gmail.com>
2014-09-23 17:50:05 -0600

    figure out termination condition

--- ass1/memchunk.c
+++ ass1/memchunk.c
@@ -14,6 +14,7 @@
  * - memchunk.length will always be >= getPageSize()
  * - memchunk.length will always be == 0 in (mod getPageSize())
  * - no contiguous memchunks in the return array have the same RW
+ * - there may be more addresses than 2^32
  * - page size may not divide 2^32 evenly
  * - iterating to 2^32 requires a 64 bit counter, but that cannot be
  *   cast to a pointer
@@ -23,9 +24,11 @@
  * General approach:
  *
  * Set a pointer to the first address of each page in the address space.
- * For each of those addresses, try accessing, then reading, then writing.
+ * For each of those addresses, try accessing, then reading, then
+ * writing.
  * If a segfault is intercepted, record the permission level.
- * If the permission level is different from the previos one, then a new chunk
+ * If the permission level is different from the previos one, then a new
+ * chunk
  * has been found at the current page:
  *     - Set the final length of the current chunk
  *     - Move to the next chunk
@@ -48,49 +51,66 @@ jmp_buf env;
 /* Handler function intended for read-triggered segfaults */
 void read_err_handler(int sig)
 {
-    longjmp(env, NOACC); /* No access to address was possible */
+        longjmp(env, NOACC); /* No access to address was possible */
 }
 
 /* Handler function intended for write-triggered segfaults */
 void write_err_handler(int sig)
 {
-    longjmp(env, READ); /* Only read access to address was possible */
+        longjmp(env, READ); /* Only read access to address was possible */
 }
 
 int get_mem_layout(struct memchunk *chunk_list, int size)
 {
-    /* Declare and initialize constants */
-    //TODO: CONVERT LONG LONG BOUNDARY AND COUNTER TO CHAR POINTER.  KEEP IN MIND POINTER ARITHMETIC AND OVERFLOW POSSIBILITY.
-    const unsigned long long MAX_ADDR = 4294967295LU;
-    const int PAGE_SIZE = getpagesize();
-    
-    unsigned long long currAddr = 0LU;
+        /* Declare and initialize constants */
+        const char * MAX_ADDR = (char *) 4294967295LU;
+        const int PAGE_SIZE = getpagesize();
+        
+        char * currAddr = (char *) 0;
 
-    for (; currAddr <= MAX_ADDR; currAddr += PAGE_SIZE) {
-        /* Set the jump point with the current stack */
-        int permission = setjmp(env);
-        /* Switch on values from error handlers for long jump. */
-        switch(permission)
+        /* All pages below MAX_ADDR have been checked if the current address
+         * is within PAGE_SIZE of MAX_ADDR.  Consider the following, where 14
+         * addresses are checked (MAX_ADDR == 14) with PAGE_SIZE == 4:
+         *
+         * Address numbers:   0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
+         * Checked addresses: *  *  *  *  *  *  *  *  *  *  *  *  *  *
+         * Page numbers:      1  1  1  1  2  2  2  2  3  3  3  3  4  4  4  4
+         *
+         * Page number 4 (addr 12) must checked, but assume the address counter
+         * (currAdd) will wrap back to zero after 13.  Then the following loop
+         * termination condition will never halt:
+         *     currAddr <= MAX_ADDR
+         *
+         * So, we need to check for the second-to-last page in the body of the
+         * loop, and perform the loop body one time afterwards
+         */
+        //TODO change the loop structure to reflect above comment
+        for (; currAddr <= MAX_ADDR; currAddr += PAGE_SIZE)
         {
-            case UNKNOWN: /* Page is not yet analyzed */
-                /* Set segfault handler to read error handler */
+                /* Set the jump point with the current stack */
+                int permission = setjmp(env);
+                /* Switch on values from error handlers for long jump. */
+                switch(permission)
+                {
+                        case UNKNOWN: /* Page is not yet analyzed */
+                                /* Set segfault handler to read error handler */
 
-                /* Try to read from current address */
+                                /* Try to read from current address */
 
-                /* Set segfault handler to write error handler */
+                                /* Set segfault handler to write error handler */
 
-                /* Try to write to current address */
+                                /* Try to write to current address */
 
-                /* If execution has reached here, the current page is
-                 * readable and writable */
-                break;
-            case NOACC: /* Page is not accessible */
+                                /* If execution has reached here, the current page is
+                                 * readable and writable */
+                                break;
+                        case NOACC: /* Page is not accessible */
 
-                break;
-            case READ: /* Page has read access but no write access */
-                break;
+                                break;
+                        case READ: /* Page has read access but no write access */
+                                break;
+                }
         }
-    }
-    
-    return 0;
+        
+        return 0;
 }
