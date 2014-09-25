ad9b9048ff881eea5a94f77abca4477668114528
kdbanman <kirby.banman@gmail.com>
2014-09-25 14:04:42 -0600

    properly use sigsetjmp and siglongjmp

--- lab/signal/sigaction.c
+++ lab/signal/sigaction.c
@@ -1,4 +1,5 @@
 #include <signal.h>
+#include <setjmp.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -6,31 +7,50 @@ typedef enum { false, true } bool;
 
 bool err;
 
-void ouch(int sig)
+jmp_buf env;
+
+void handler(int sig)
 {
-    printf("signal: %d\n", sig);
+    printf("signal received: %d\n", sig);
     err = true;
+
+    siglongjmp(env, 1);
+    
+    printf("here\n");
 }
 
 int main()
 {
-    err = false;
-
+    /* config and register segfault handler */
     struct sigaction act;
-
-    act.sa_handler = ouch;
+    
+    act.sa_handler = handler;
     sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
 
     sigaction(SIGINT, &act, 0);
 
+    err = false;
+
+    sigsetjmp(env, 1);
+
+    printf("post setjmp\n");
+
+    int i = 0;
     while (1)
     {
-        if (!err) {
-            printf("nonblock?\n");
-        } else {
-            printf("nonblock? sigint recieved\n");
+        i++;
+
+        /* cause segfault every third iteration*/
+        if (i % 3 == 0) {
+            printf("raising signal\n");
+            raise(SIGINT);
         }
+
+        printf("%d\n", i);
+
+        err = false;
+
         sleep(1);
     }
 }
