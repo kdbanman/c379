b8df878bdc0bdd8e555e7426d74722eab83a8006
kdbanman <kdbanman@ualberta.ca>
2014-10-10 10:09:42 -0600

    prototype some utilities. make notes.

--- ass2/server_util.h
+++ ass2/server_util.h
@@ -8,3 +8,35 @@
  *
  */
 
+struct serverconf {
+        int port;
+        char * basedir;
+        char * logloc;
+};
+
+/*
+ * Parse and validate command line arguments to obtain runtime configuration.
+ * Invalid arguments terminate program with error code 1.
+ */
+struct serverconf getConfig(int argc, char *argv[]);
+
+/*
+ * Returns nonzero (true) if the passed port is between 0 and 65535 inclusive.
+ */
+int validPort(int port);
+
+/*
+ * Returns nonzero (true) if the passed string is a valid directory or file.
+ */
+int validPath(char * path);
+
+/*
+ * Returns nonzero (true) if the passed string is a readable directory.
+ */
+int validBaseDir(char * path);
+
+/*
+ * Returns nonzero (true) if the passed string is a writable file.
+ */
+int validLogFile(char * path);
+
